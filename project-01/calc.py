30620cab934b9eead812b9fabd02c8bb1a3f19e9
Boiiterra <kostyaborisenko05@gmail.com>
2023-10-09 00:54:34 +0300

    We did the job late

--- project-01/calc.py
+++ project-01/calc.py
@@ -1,10 +1,69 @@
-from converter import convI, convF, decI, decF
+from converter import conv
+from math import cos, sin
 
 
-def sep():
-    return print("-" * 8)
+def knormal(num: str):
+    num = num.replace(",", ".")
+    if "." in num:
+        return num
+    return num + ".0"
 
 
-for i in range(2, 37):
-    num = ".09868632579000"
-    print(f"{num} in {i} -> {convF(num, 10, i)}")
+def addition(a, b, rss: int):
+    c = a + b
+    if c < 0:
+        return "-" + conv(knormal(str(-c)), 10, rss)
+    return conv(knormal(str(c)), 10, rss)
+
+
+def subtraction(a, b, rss: int):
+    c = a - b
+    if c < 0:
+        return "-" + conv(knormal(str(-c)), 10, rss)
+    return conv(knormal(str(c)), 10, rss)
+
+
+def multiplication(a, b, rss: int):
+    c = a * b
+    if c < 0:
+        return "-" + conv(knormal(str(-c)), 10, rss)
+    return conv(knormal(str(c)), 10, rss)
+
+
+def division(a, b, rss: int):
+    if b == 0:
+        return None
+    c = a / b
+    if c < 0:
+        return "-" + conv(knormal(str(-c)), 10, rss)
+    return conv(knormal(str(c)), 10, rss)
+
+
+def sine(a, rss):
+    c = sin(a)
+    if c < 0:
+        return "-" + conv(knormal(str(-c)), 10, rss)
+    return conv(knormal(str(c)), 10, rss)
+
+
+def cosine(a, rss):
+    c = cos(a)
+    if c < 0:
+        return "-" + conv(knormal(str(-c)), 10, rss)
+    return conv(knormal(str(c)), 10, rss)
+
+
+def kraise(a, b, rss):
+    c = a**b
+    if c < 0:
+        return "-" + conv(knormal(str(-c)), 10, rss)
+    return conv(knormal(str(c)), 10, rss)
+
+
+def root(a, b, rss):
+    if b == 0:
+        return None
+    c = a ** (1 / b)
+    if c < 0:
+        return "-" + conv(knormal(str(-c)), 10, rss)
+    return conv(knormal(str(c)), 10, rss)
--- project-01/converter.py
+++ project-01/converter.py
@@ -1,17 +1,30 @@
-alphabet = "".join(
-    [str(el) for el in range(10)] + [chr(el)
-                                     for el in range(ord("A"), ord("Z") + 1)]
-)
+alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# alphabet = "".join(
+#     [str(el) for el in range(10)] + [chr(el)
+#                                      for el in range(ord("A"), ord("Z") + 1)]
+# )
+
+
+def dec(num: str, from_base: int):
+    return decI(num, from_base) + decF(num, from_base)
+
+
+def conv(num: str, from_base: int, new_base: int):
+    return convI(num, from_base, new_base) + convF(num, from_base, new_base)
 
 
 def decI(num: str, from_base: int):
+    m = -1 if (num[0] == "-") else 1
+    num = num.replace(",", ".").split(".")[0]
+    if m < 0:
+        num = num[1:]
     num = num[::-1]
     r = 0
 
     for pos, dig in enumerate(num):
         r += alphabet.index(dig) * from_base**pos
 
-    return r
+    return m * r
 
 
 def decF(num: str, from_base: int):
@@ -25,18 +38,23 @@ def decF(num: str, from_base: int):
 
 
 def convI(num: str, from_base: int, new_base: int):
+    m = "-" if (num[0] == "-") else ""
+    if m:
+        num = num[1:]
     res: list[str] = []
     num = decI(num, from_base)
     while num:
         res.append(alphabet[num % new_base])
         num //= new_base
-    return "".join(res[::-1])
+
+    if not res:
+        return "0"
+    return m + "".join(res[::-1])
 
 
 def convF(num: str, from_base: int, new_base: int, precision: int = 10):
     res: list[str] = []
     num = decF(num, from_base)
-    _ = new_base
     for _ in range(precision):
         tmp = num * new_base
         num = tmp - int(tmp)
